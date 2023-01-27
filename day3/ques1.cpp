4bd08ce0416181a66bea100b804f2cff766567ca
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-01-28 00:40:22 +0530

    Create ques1.cpp

---
+++ day3/ques1.cpp
@@ -0,0 +1,22 @@
+/*https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/ 
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+    int maximumCount(vector<int>& nums) {
+        int pi = 0;
+        int ni = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] < 0 )ni++;
+            else if(nums[i] >0)pi++;
+        }
+
+        return (ni > pi ? ni : pi);
+    }
+
+int main(){
+
+    return 0;
+}
