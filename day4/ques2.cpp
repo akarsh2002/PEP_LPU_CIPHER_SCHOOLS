e68a701fa190a10d1dc8757fc2232fe6ef253cde
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-01-28 01:18:29 +0530

    Add files via upload

---
+++ day4/ques2.cpp
@@ -0,0 +1,15 @@
+/*rummimg sum*/
+#include<bits/stdc++.h>
+
+using namespace std;
+vector<int> runningSum(vector<int>& nums) {
+    for(int i = 1;i<nums.size();i++){
+        nums[i]+=nums[i-1];
+    }    
+    return nums;
+}
+
+int main(){
+
+    return 0;
+}
