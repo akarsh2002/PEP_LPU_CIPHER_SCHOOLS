b255bc621340cb0d286dba0b58851ab004ccf93c
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-01-28 13:45:52 +0530

    Update ques3.cpp

--- day6/ques3.cpp
+++ day6/ques3.cpp
@@ -1,44 +1,31 @@
+/*rotate the array*/
 #include<bits/stdc++.h>
 
 using namespace std;
 
-// approach 1 O(n^2)
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int>ans;
-        for(int i = 1;i<=nums.size();i++){
-            bool f = 0;
-            for(int j = 0;j<nums.size();j++){
-                if(nums[j] == i){
-                    f = 1;
-                    break;
-                }
-            }
-            if(!f){
-                ans.push_back(i);
-            }
-        }
+//approach 1 TC: O(n) SC: O(n)
+/*void rotateArr(vector<int>&v,int r){
+//     vector<int>ans(v.size());
+//     for(int i = 0;i<v.size();i++){
+//         ans[(i+r)%ans.size()] = v[i];
+//     }
+//     for(int i = 0;i<ans.size();i++)cout<<ans[i]<<' ';
+//     cout<<endl;
+//     return;
+ }*/
 
-        return ans;
-    }
-
-// approach 2 O(n)
-vector<int> findDisappearedNumbers(vector<int>& nums) {
-
-    vector<int>ans;
-    for(int i = 0;i<nums.size();i++){
-        if(nums[abs(nums[i])-1] > 0 ){
-            nums[nums[i]-1]*=-1;
-        }
-    }
-
-    for(int i = 0;i<nums.size();i++){
-        if(nums[i] > 0)ans.push_back(i+1);
-    }
-
-    return ans;
+// approach 2
+void rotateArr(vector<int>&v,int r){
+    reverse(v.begin(),v.end());
+    reverse(v.begin(),v.begin()+r);
+    reverse(v.begin()+r,v.end());
+    for(int i = 0;i<v.size();i++)cout<<v[i]<<" ";
+    cout<<endl;
 }
 
 int main(){
 
+    vector<int>v = {4,3,2,7,8,2};
+    rotateArr(v,2);
     return 0;
 }
