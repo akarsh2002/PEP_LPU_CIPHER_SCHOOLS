052bd91a41617c01c477d895b08b55e9ce6f835e
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-02-02 23:14:25 +0530

    Create ques1.cpp

---
+++ day12/ques1.cpp
@@ -0,0 +1,15 @@
+/*https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    int numberOfCuts(int n) {
+        if(n == 1)return 0;
+        return (n%2 == 0) ? n/2 : n;
+    }
+
+int main(){
+
+    return 0;
+}
