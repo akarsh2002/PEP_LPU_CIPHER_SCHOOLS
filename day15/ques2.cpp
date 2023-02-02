adedb0467d8a7989309df248569dd4493c519b53
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-02-03 02:11:20 +0530

    Create ques2.cpp

---
+++ day15/ques2.cpp
@@ -0,0 +1,17 @@
+/*sum of digits using recursion*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumOfDigit(int num){
+    if(num <= 9)return num;
+
+    int s = sumOfDigit(num/10);
+    
+    return (s+(num%10));
+}
+
+int main(){
+
+    return 0;
+}
