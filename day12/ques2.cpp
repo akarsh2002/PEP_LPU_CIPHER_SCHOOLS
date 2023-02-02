58e1f09aa996715f88ce127fe6028ae451909364
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-02-02 23:16:10 +0530

    Create ques2.cpp

---
+++ day12/ques2.cpp
@@ -0,0 +1,18 @@
+/*https://leetcode.com/problems/replace-all-digits-with-characters/*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    string replaceDigits(string s) {
+        for(int i = 1;i<s.size();i+=2){
+            s[i] = s[i-1] + (s[i] - '0');
+        }
+
+        return s;
+    }
+
+    
+int main(){
+
+    return 0;
+}
