198db4a2490052dbeb86395d51af34051bece2ca
Akarsh Anand <73278292+akarsh2002@users.noreply.github.com>
2023-02-02 22:57:17 +0530

    Create ques1.cpp

---
+++ day10/ques1.cpp
@@ -0,0 +1,19 @@
+/*https://leetcode.com/problems/defanging-an-ip-address/*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(int i = 0;i<address.size();i++){
+            if(address[i]== '.')ans+="[.]";
+            else ans+=address[i];
+        }   
+
+        return ans;
+    }
+
+int main(){
+
+    return 0;
+}
